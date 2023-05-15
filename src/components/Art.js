5db50016041eb8ab95abee891a912a45a7d82748
jamesonpastor <jpasto@uw.edu>
2023-05-15 14:02:03 -0700

    Made student cards clickable

---
+++ src/components/Art.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Cards from './Cards';
+import NavBar from './NavBar';
+import { Link } from 'react-router-dom';
+import LanguageSelector from './LanguageSelector';
+
+function Art() {
+  return (
+    <div className="art">
+      <h1>Art</h1>
+      <Link 
+        title="Game 1"
+        description="This is the description for Game 1"/>
+      <Link
+        title="Game 2"
+        description="This is the description for Game 2"/>
+      <Link
+        title="Learning Resource"
+        description="This is the description for Resource 1"/>
+      <Link
+        title="Learning Resource"
+        description="This is the description for Resource 2"/>
+    </div>
+  );
+}
+
+export default Art;
