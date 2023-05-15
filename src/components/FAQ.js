643c5fc6f1e8d1e7cad1363e38ea70c1dd645424
jamesonpastor <jpasto@uw.edu>
2023-05-15 13:50:04 -0700

    Edited FAQ

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -6,7 +6,11 @@ function FAQ() {
     <div>
       <NavBar/>
       <h1>Frequent Asked Questions</h1>
-      <p>*something about why we are interested in equality in education*!</p>
+      <p>We believe that addressing the educational inequities faced by students living in poverty is a critical issue. 
+        By providing free access to educational resources, we can help level the playing field and improve academic outcomes for disadvantaged students. 
+        Education is essential for breaking the cycle of poverty and increasing upward socioeconomic mobility. 
+        Additionally, providing equitable education opportunities can help reduce inequalities across race, class, and gender. 
+        Measuring outcomes through standardized test scores and student satisfaction surveys can help determine the effectiveness of our solution and guide future improvements.</p>
     </div>
   );
 }
