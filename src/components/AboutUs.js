74287d440f5483d8a49bd461a5dcd2984c6d4fc5
sydneyimokawa <simokawa@uw.edu>
2023-05-17 15:29:31 -0700

    embedded video & set up FAQ

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import NavBar from './NavBar';
+import VIDEO from './img/VIDEO.mp4';
+
+
 
 function AboutUs() {
   return (
     <div>
       <NavBar/>
       <h1>About Us</h1>
-      <p>*something about why we are interested in equality in education*!</p>
+
+        <h2>
+          UN SDG 4: Quality Education
+        </h2>
+
+        <p>
+          Our goal is to address the fourth UN SDG of Quality Education by providing elementary students below the poverty line in 
+          Washington State with free access to educational resources, such as books, technology, and learning materials. We aim to alleviate 
+          the disadvantage faced by low-income families due to limited access to resources and funding, which can hinder academic performance 
+          and upward socioeconomic mobility. Our solution seeks to promote equity in education and reduce inequalities across race, class, 
+          and gender. We will measure the success of our solution through standardized test scores and student satisfaction surveys.
+        </p>
+
+        
+        <div >
+            <h2 ><cite><a
+                href="https://www.youtube.com/watch?v=4HXyJmY--gM">Understand Goal 4: Quality Education</a></cite></h2>
+            
+            <video controls>
+                <source src= {VIDEO} type="video/mp4" />
+            </video>
+        </div>
+        
     </div>
   );
 }
--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -5,12 +5,24 @@ function FAQ() {
   return (
     <div>
       <NavBar/>
-      <h1>Frequent Asked Questions</h1>
+      <h1>Frequently Asked Questions</h1>
       <p>We believe that addressing the educational inequities faced by students living in poverty is a critical issue. 
         By providing free access to educational resources, we can help level the playing field and improve academic outcomes for disadvantaged students. 
         Education is essential for breaking the cycle of poverty and increasing upward socioeconomic mobility. 
         Additionally, providing equitable education opportunities can help reduce inequalities across race, class, and gender. 
         Measuring outcomes through standardized test scores and student satisfaction surveys can help determine the effectiveness of our solution and guide future improvements.</p>
+
+        <div class="faq-container">
+          <div class="faq">
+            <h2 class="question">Question 1</h2>
+            <p class="answer">Answer 1</p>
+          </div>
+          <div class="faq">
+            <h2 class="question">Question 2</h2>
+            <p class="answer">Answer 2</p>
+          </div>
+          
+        </div>
     </div>
   );
 }
