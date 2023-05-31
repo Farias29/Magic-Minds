7dbdfb91466a09bc7808675d7ba0a795dd09e045
Farias29 <Farias29@uw.edu>
2023-05-31 00:56:18 -0700

    added FAQ updates in spanish

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -46,14 +46,14 @@ function FAQ() {
                 <p>
                   While younger students may not yet understand the benefits of homework, it is important to support expectations 
                   surrounding homework. Homework cements learning done in the classroom and helps build a sense of responsibility. 
-                  Parents can help by creating a productive and conducive study environment for they're students, as well as by setting 
-                  clear expectations and establsih consistent routines. This will help build good habits from a young age.
+                  Parents can help by creating a productive and conducive study environment for their students, as well as by setting 
+                  clear expectations and establish consistent routines. This will help build good habits from a young age.
                 </p>
                 <li className='tip'>Set Your Student Up For Success!</li>
                 <p>
                   It's no secret that breakfast is the most important meal of the day. Support your student by making sure they 
                   are prepared for the day, both physically and mentally! A hearty, nutritious breakfast and a good night's rest 
-                  go a long way in helping student's succeed while at school.
+                  go a long way in helping students succeed while at school.
                 </p>
               </ol>
             </div>
@@ -87,16 +87,42 @@ function FAQ() {
             tener éxito!</p>
           </div>
           <div className="faq">
-            <h2 className="question">¿Cómo puedo apoyar mejor a mi hijo?</h2>
-            <p className="answer"> </p>
-          </div>
-          <div className="faq">
-            <h2 className="question">¿Por qué debería usar Mentes Mágicas?</h2>
-            <p className="answer"> </p>
-          </div>
-          
+              <h2 className="question">¿Cómo puedo apoyar mejor a mi hijo?</h2>
+              <p className="answer">Aquí en Mentes Magicas, creemos en la educación equitativa y el éxito para todos los estudiantes.
+              Nos aseguramos de que su estudiante tenga el apoyo que necesita porque creemos que es crucial para el éxito tanto en la
+              clase como en la vida. ¡Aquí hay algunos consejos para ayudarlo a apoyar a su estudiante lo mejor que pueda!</p>
+              <ol>
+                <li className='tip'>Asistir a la junta de regreso a clases y a las conferencias de padres y maestros </li>
+                <p>Participar en la educación de su hijo es una manera  excelente de apoyarlo. Este tipo de eventos escolares pueden ayudar
+                  a los padres a mantenerse informados sobre el progreso de la educación de sus hijos y cualquier programa especial que ofrezca
+                  la escuela. Las reuniones con los profesores es una manera excelente para conocer al educador de su hijo y cualquier expectativa
+                  que puedan tener. También proporcionan un canal con el que los padres puedan placticar como ayudar mejor a sus estudiantes.
+                </p>
+                <li className='tip'>Reforzar las expectativas en torno a la tarea</li>
+                <p>
+                Si bien es posible que los estudiantes más jóvenes aún no entiendan los beneficios de la tarea, es importante apoyar las expectativas
+                que rodean la tarea. La tarea consolida el aprendizaje realizado en el aula y ayuda a construir un sentido de responsabilidad. Los padres
+                pueden ayudar creando un ambiente de estudio productivo y propicio para sus estudiantes, así como estableciendo expectativas claras y
+                estableciendo rutinas consistentes. Esto ayudará a construir buenos hábitos desde una edad temprana.
+                </p>
+                <li className='tip'>¡Prepara a tu estudiante para el éxito!</li>
+                <p>
+                No es ningún secreto que el desayuno es la comida más importante del día. ¡Apoye a su estudiante asegurándose de que esté preparado para
+                el día, tanto físicamente como mentalmente! Un desayuno abundante y nutritivo y una buen descanso ayudan mucho a los estudiantes a tener
+                éxito mientras están en la escuela.
+                </p>
+              </ol>
+            </div>
+            <div className="faq">
+              <h2 className="question">¿Por qué debería usar Mentes Mágicas?</h2>
+              <p className="answer">Mentes Mágicas está aquí para ayudar. Entendemos la importancia de la educación de los jóvenes, pero también entendemos
+              lo difícil que puede ser apoyar académicamente a los jóvenes estudiantes, especialmente para las familias de bajos ingresos. Queremos ayudar a
+              cerrar la brecha en la educación de la información y los recursos. Tenemos recursos para que los estudiantes los ayuden a practicar sus habilidades
+              fuera del aula y fomentar la progresión del aprendizaje. También proporcionamos recursos para padres y familias, para que tengan el conocimiento
+              para apoyar mejor a sus estudiantes. </p>
+            </div>
         </div>
-        </div>
+    </div>
     </div>
     
   );  
